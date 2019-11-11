6af49eb0748f25a67c38f81374301e1112c576b5
wulantoro <nugrahawulantoro@gmail.com>
2019-11-11 09:43:28 +0700

    sync sap 11-11-19

--- application/models/M_uploadsap.php
+++ application/models/M_uploadsap.php
@@ -14,7 +14,7 @@ function read($workCenter) {
 		return $query->result_array();
 }
 
-/*********************berner8******************/
+/*********************berner8 lama******************/
 	// function addHeader($data) {
 		
 		// $this->db->insert('STEM_MOBILE_SHOPFLOORHEADER', $data);
@@ -23,55 +23,6 @@ function read($workCenter) {
 		
 
 	// }
-
-
-// /******************NYOBA INSERT 3 TABEL SEKALIGUS*************salah********************/
-/*function addHeader($header, $criteria, $reject) {
-	$this->db->trans_start();
-
-	 // $id = $this->input->post('id');
-	 // $docNum = $this->input->post('docNum');
-	 // $prodNo = $this->input->post('prodNo');
-
-	// insert to gheader
-	$data = array(
-		'docNum' => $header,
-		'prodNo' => $header,
-		'id'	=>  $header,
-);
-$this->db->insert('STEM_MOBILE_SHOPFLOORHEADER', $data);
-
-//get docentry header
-$docEntry_header = $this->db->insert_id();
-$result = array();
-// 	foreach($reject AS $key => $val) {
-// 		$result[] = array(
-// 			'hostHeadEntry' => $docentry_header,
-// 			'hostHeadEntry' => $_POST['STEM_MOBILE_SHOPFLOORLINESCRITERIA'][$key]
-// 			);
-// } 
-
-//MULTIPLE INSERT TO DETAIL TABLE
-$this->db->insert_batch('STEM_MOBILE_SHOPFLOORLINESREJECT', $result);
-$this->db->trans_complete();
-
-}*/
-
-/*******************coba terus ninput 3 tabvle****************** salah/
-// function create ($data1, $data2) {
-// 	$this->db->insert( $data1);
-// 	$id_table1 = $this->db->insert_id();
-
-// 	array_unshift($data2, array('hostHeadEntry' => $id_table1 ));
-
-// 	$this->db->insert( $data2);
-// 	$id_table2 = $this->db->insert_id();
-
-// 	$return_data = array($data1 => $id_table1, $data2 => $d_table2);
-
-// 	return $return_data;
-// }
-
 
 
 /******************NYOBA lagi dan lagi INSERT 3 TABEL SEKALIGUS******************************BENER***/
@@ -90,8 +41,8 @@ $this->db->trans_complete();
 // 	$sql2 = "INSERT INTO STEM_MOBILE_SHOPFLOORLINESREJECT (hostHeadEntry, lineNumber, rejectCode, id, rejectName, rejectQty) VALUES ($docEntry_header,?,?,?,?,?)";
 // 	$this->db->query($sql2, $data2);
 
-// 	// $sql3 = "INSERT INTO STEM_MOBILE_SHOPFLOORLINESCRITERIA (hostHeadEntry, id, lineNumber, criteria, criteriaDesc, standard, actualResult, actualRemarks) VALUES ($docEntry_header,?,?,?,?,?,?,?)";
-// 	// $this->db->query($sql3, $data3);
+// // 	// $sql3 = "INSERT INTO STEM_MOBILE_SHOPFLOORLINESCRITERIA (hostHeadEntry, id, lineNumber, criteria, criteriaDesc, standard, actualResult, actualRemarks) VALUES ($docEntry_header,?,?,?,?,?,?,?)";
+// // 	// $this->db->query($sql3, $data3);
 
 // 	$this->db->trans_complete(); 
 
@@ -101,29 +52,64 @@ $this->db->trans_complete();
 /*****************************************************************/
 
 
+// function addHeader($data1, $data2) {
+//   $this->db->trans_start();
 
-/*****************************input array*************************/
+//   $this->db->insert('STEM_MOBILE_SHOPFLOORHEADER', $data1);
+//   $docEntry_header = $this->db->insert_id();
+
+//   for($i = 0; $i < count($data2); $i++) {
+//     $data2[$i]['hostHeadEntry'] = $docEntry_header;
+//   }
+
+// 	 $this->db->insert_batch('STEM_MOBILE_SHOPFLOORLINESREJECT', $data2);
+
+//    $dataArr = array($data1, $data2);
+
+//    if ($this->db->trans_status() === FALSE) {
+//      # something went wrong
+//     $this->db->trans_rollback();
+//     return FALSE;
+//    } else {
+//     # everithing is perfect
+//     $this->db->trans_commit();
+//     return $dataArr;
+    
+//    }
+
+// }
+
+
+
+/*********************************************************/
+
+
 function addHeader($data1, $data2) {
-	$this->db->trans_start();
+  $this->db->trans_start();
 
-	$sql1 = "INSERT INTO STEM_MOBILE_SHOPFLOORHEADER (id, docNum, prodNo, mobileId, docDate, prodCode, prodName, prodPlanQty, prodStatus, routeCode, routeName, sequence, sequenceQty, shift, shiftName, tanggalMulai, tanggalSelesai, jamMulai, jamSelesai, inQty, outQty, remarks, userId, posted, TargetEntry, UploadTime, workCenter, status) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+  $this->db->insert('STEM_MOBILE_SHOPFLOORHEADER', $data1);
+  $docEntry_header = $this->db->insert_id();
 
-	$this->db->query($sql1, $data1);
-	$docEntry_header = $this->db->insert_id();
+  for($i = 0; $i < count($data2); $i++) {
+    $data2[$i]['hostHeadEntry'] = $docEntry_header;
+  }
 
-	$sql2 = $this->db->insert_batch('STEM_MOBILE_SHOPFLOORLINESREJECT', $data2, ['hostHeadEntry' => $docEntry_header]); 
+	$this->db->insert_batch('STEM_MOBILE_SHOPFLOORLINESREJECT', $data2);
 
-	$this->db->trans_complete(); 
+   $dataArr = array($data1, $data2);
 
- return $this->db->insert_id(); 
+   if ($this->db->trans_status() === FALSE) {
+     # something went wrong
+    $this->db->trans_rollback();
+    return FALSE;
+   } else {
+    # everithing is perfect
+    $this->db->trans_commit();
+    return $dataArr;
+    
+   }
+
 }
-
-/********************************************************/
-
-
-
-	/****************************************************************/
-
 
 function putHeader($data, $docEntry) {
 	 // $this->db->update('IPP_MOBILE_SHOPFLOORHEADER', $data,['docEntry'=> $docEntry]);
